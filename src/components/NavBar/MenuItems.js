4f12025eca0e17c7bdd869d9e4e0ef7730439bb6
dhifafaz <dhifaf.az@gmail.com>
2021-01-28 22:55:16 +0700

    commit

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,42 @@
+export const MenuItems = [
+    {
+        title: 'Faculty Homepage',
+        path : '/faculty-homepage',
+        cName :'dropDownLink'
+    },
+    {
+        title: 'Scele 2015',
+        path : '/Scele-2015',
+        cName :'dropDownLink'
+    },
+    {
+        title: 'Website UI',
+        path : '/Website-UI',
+        cName :'dropDown-Link'
+    },
+    {
+        title: 'Center for Learning Resouce UI',
+        path : '/Center-for-Learning-Resouce-UI',
+        cName :'dropDown-Link'
+    },
+    {
+        title: 'Perpustakaan UI',
+        path : '/Perpustakaan-UI',
+        cName :'dropDown-Link'
+    },
+    {
+        title: 'Katalog Perpustakaan UI',
+        path : '/Katalog-Perpustakaan-UI',
+        cName :'dropDown-Link'
+    },
+    {
+        title: 'Siak',
+        path : '/Siak',
+        cName :'dropDown-Link'
+    },
+    {
+        title: 'Portal Apps',
+        path : '/Portal-Apps',
+        cName :'dropDown-Link'
+    }
+];
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-//import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
@@ -9,6 +9,7 @@ import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import './NavBar.css';
+import { MenuItems } from './MenuItems';
 
 function NavigationBar (){
 	const [show, setShow] = useState(false);
@@ -18,37 +19,40 @@ function NavigationBar (){
 	const hideDropdown = props => {
 		setShow(false);
 	}
-
+	
 	return (
 
-	<header className="navBarContainer">
+<header className="navBarContainer">
 	<Navbar className="navBar" expand="lg">
 	<Navbar.Brand className="home" href="#home">SCELE</Navbar.Brand>
 	<Navbar.Toggle className="toggleButton" aria-controls="basic-navbar-nav" />
 	<Navbar.Collapse  id="basic-navbar-nav">
 		<Nav className="mr-auto navBarInner">
 			<NavDropdown 
-				title="Dropdown" 
-				id="nav-dropdown" 
+				title="Academic Links" 
+				id="navDropdown" 
 				show={show}
 				onMouseEnter={showDropdown} 
 				onMouseLeave={hideDropdown}
-				>
-			<div className="items">
-				<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-				<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-				<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-				<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-				</div>
+			>
+			{MenuItems.map((item, index) => {
+				return(
+					<NavDropdown.Item as='li' key={index} className="dropdownItem item">
+					<Link>
+						{item.title}
+					</Link>
+					</NavDropdown.Item>
+				);
+			})}
 			</NavDropdown>
 		</Nav>
 		<Form inline>
 			<FormControl type="text" placeholder="Search" className="mr-sm-2" />
 			<Button variant="outline-success">Search</Button>
 		</Form>
-	</Navbar.Collapse>
-</Navbar>
-	</header>
+		</Navbar.Collapse>
+	</Navbar>
+</header>
 		
 	);
 	
